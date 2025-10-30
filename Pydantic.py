ec642782b94f18d22c1b963ceeb050735710108f
Bahram Durani <bahramdr9@gmail.com>
2025-10-30 14:43:03 +0500

    Add files via upload

--- Pydantic.py
+++ Pydantic.py
@@ -1,25 +1,43 @@
 
 # Importing Pydantic base class for data validation
 
-from pydantic import BaseModel  # Basemodel checks for is data valid or not 
+from pydantic import BaseModel, EmailStr  
+from typing import List, Dict , Optional 
 
 class Patient(BaseModel):  # Creating a data model using Pydantic
-    name: str  #  name must be a string
-    age: int   #  age must be an integer
+    name: str 
+    age: int
+    email: EmailStr   
+    weight: float 
+    married: Optional[bool]  = None 
+    allergies: Optional[List[str]]   = None  # default value is None
+    contact_details: Dict[str, str]      # key --> str,  value --> str 
+
 
 def insert_patient_data(patient: Patient):  # Function that accepts an object
-    print(patient.name)  # Accessing name from the Patient model
-    print(patient.age)   # Accessing age from the Patient model
-    print('inserted')    # Confirmation message
+    print(patient.name)  
+    print(patient.age) 
+    print(patient.email)
+    print(patient.weight)
+    print(patient.married) 
+    print(patient.allergies)  
+    print('inserted')   
 
 
 def update_patient_data(patient: Patient):  # Function that accepts an object
-    print(patient.name)  # Accessing name from the Patient model
-    print(patient.age)   # Accessing age from the Patient model
-    print('updated')    # Confirmation message
+    print(patient.name) 
+    print(patient.age) 
+    print(patient.email)
+    print(patient.weight)
+    print(patient.married) 
+    print(patient.allergies)
+    print('updated')    
 
 
-patient_data = {'name': 'Bahram', 'age': 24}  # Raw input dictionary (like API input)
+patient_data = {'name': 'Bahram', 'age': 24, 'email': 'xy@gmail.com', 'weight': 44.9, 'married': False, 
+                'contact_details':  {'Email': 'bah13@gmail.com' , 'phone': '878734'} } 
+
+
 patient1 = Patient(**patient_data)  # we write ** to unpack the dictionary  
 
 # insert_patient_data(patient1)  # Passing validated data into function
